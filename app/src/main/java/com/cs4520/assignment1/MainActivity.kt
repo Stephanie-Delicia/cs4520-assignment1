e3d0ffcfa51f825110919b06cb92a9741daa1d2d
Stephanie M <martinez.s@northeastern.edu>
2024-03-11 21:13:18 -0400

    ProgressBar comes and goes as it should. Now need to show its actually making progress

--- app/src/main/java/com/cs4520/assignment1/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -3,6 +3,17 @@ package com.cs4520.assignment1
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
 import com.cs4520.assignment1.RetrofitClient.retrofit
@@ -24,9 +35,11 @@ class MainActivity : FragmentActivity() {
         val service = retrofit.create(ApiService::class.java)
 
         button.setOnClickListener {
+            // setContent { IndeterminateCircularIndicator()}
             CoroutineScope(Dispatchers.IO).launch {
-
-                val response = service.getAllData("1")
+                // setContent {IndeterminateCircularIndicator()}
+                // setContent { IndeterminateCircularIndicator()  }
+                val response = service.getAllData("2")
                 if (response.isSuccessful) {
                     // need to filter the list of data
                     // if empty, give toast error
@@ -41,9 +54,26 @@ class MainActivity : FragmentActivity() {
                     }
                 } else {
                     Log.i("API Call:", "Failed to fetch data.")
-                }
 
+                }
             }
         }
     }
+}
+
+@Composable
+fun IndeterminateCircularIndicator() {
+    var loading by remember {
+        mutableStateOf(true) // initialize as true to first display progress
+    }
+
+    if (loading) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    } else {return}
+
+    loading = !loading
 }
--- app/src/main/java/com/cs4520/assignment1/fragments/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/fragments/LoginFragment.kt
@@ -8,17 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment1.ApiService
 import com.cs4520.assignment1.R
+import com.cs4520.assignment1.RetrofitClient
 import com.cs4520.assignment1.databinding.FragmentLoginBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
 
     private lateinit var loginBtn : Button
     private lateinit var userTextView : EditText
     private lateinit var passTextView : EditText
+    private lateinit var progressBar: ProgressBar
     private lateinit var navHostFragmentController : NavController
 
     private var _binding: FragmentLoginBinding? = null
@@ -42,19 +49,46 @@ class LoginFragment : Fragment() {
         loginBtn = binding.LoginButton
         userTextView = binding.editTextUsername
         passTextView = binding.editTextPassword
+        progressBar = binding.progressBar
 
         loginBtn.setOnClickListener{
             if ((userTextView.text.toString() == "admin") and (passTextView.text.toString() == "admin")) {
                Toast.makeText(requireActivity(), "Successful login.", Toast.LENGTH_SHORT).show()
 
-                navHostFragmentController.navigate(R.id.action_loginFragment_to_productListFragment)
-                userTextView.setText("")
-                passTextView.setText("")
+                progressBar.visibility = View.VISIBLE
+                retrieveProductData(progressBar)
+                //navHostFragmentController.navigate(R.id.action_loginFragment_to_productListFragment)
+                //userTextView.setText("")
+                //passTextView.setText("")
             } else {
                 Toast.makeText(requireActivity(), "Invalid username/password. Hint: admin", Toast.LENGTH_SHORT).show()
             }
         }
 
         return view
+    }
+
+    fun retrieveProductData(progressBar: ProgressBar) {
+        val service = RetrofitClient.retrofit.create(ApiService::class.java)
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = service.getAllData("2")
+            if (response.isSuccessful) {
+                // need to filter the list of data
+                // if empty, give toast error
+                // page 5 is empty!
+                Log.i("API Call:", response.toString())
+                val data = response.body()
+                if (!data.isNullOrEmpty()) {
+                    Log.i("Actual Data:", data.toString())
+                    Log.i("Sample Data:", data[0].getName().toString())
+                } else {
+                    Log.i("API Call:", "Empty data.")
+                }
+            } else {
+                Log.i("API Call:", "Failed to fetch data.")
+
+            }
+            progressBar.visibility = View.INVISIBLE
+        }
     }
 }
